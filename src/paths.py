0f70eef27736311a56ea98b90add33abc54de97a
PedroDavid2001 <pedrodavirochasaldanha@gmail.com>
2026-06-11 10:09:16 -0300

    final updt

---
+++ src/paths.py
@@ -0,0 +1,9 @@
+# OBS.: ALTERE PARA O CAMINHO ABSOLUTO DO ARQUIVO NA SUA MÁQUINA
+PATH_TO_MY_ONTO = (
+    "file://path/to/owlready_projects/onto_files/OntoUML_Movies.rdf"
+)
+
+#OBS.: ESSE AQUI NÃO PRECISA DE "file://"
+PATH_TO_MY_GUFO = (
+    "path/to/owlready_projects/onto_files/gUFO_RDF.rdf"
+)
